df1dae338fcc38d9582a46a3e06536f3d83a8190
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2025-03-10 22:48:19 -0300

    aula exemplo 01

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import Constants from 'expo-constants';
+import Constants from 'expo-constants'; 
+
+// Exemplos
+import Exemplo01 from './src/exemplos/exemplo-01';
+
+// Atividades
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Troquei mais uma vez!</Text>
-      <StatusBar style="auto" />
+      <Exemplo01 />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -15,9 +20,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center', 
+    backgroundColor: '#000',
+    // alignItems: 'center',
+    // justifyContent: 'center', 
     padding: 8, 
     paddingTop: Constants.statusBarHeight || 8, 
   },
---
+++ src/exemplos/exemplo-01/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'; 
+
+import styles from './styles';
+
+function Exemplo01 () {
+    return(
+        <View style={styles.container}>
+            <Text>Exemplo 1</Text>
+            <Text>Aula PAM I, primeiro componente.</Text>
+        </View>
+    );
+}
+
+export default Exemplo01;
