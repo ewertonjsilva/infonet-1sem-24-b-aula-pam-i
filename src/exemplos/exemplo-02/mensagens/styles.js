9694e6f690b1bcf9e1abd247cbdc726e0e5467ce
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2025-03-24 22:47:21 -0300

    exemplo 2 finalizado

--- src/exemplos/exemplo-02/mensagens/styles.js
+++ src/exemplos/exemplo-02/mensagens/styles.js
@@ -10,7 +10,17 @@ const styles = StyleSheet.create({
         borderRadius: RFPercentage(1.5), 
         width: '90%', 
         alignItems: 'center', 
-    },
+        marginBottom: RFPercentage(1), 
+    }, 
+    titulo: {
+        fontSize: RFPercentage(3.5), 
+        fontWeight: 'bold', 
+        color: 'darkslategrey', 
+    }, 
+    mensagem: {
+        fontSize: RFPercentage(2), 
+        color: '#222',
+    }, 
 });
 
 export default styles;
