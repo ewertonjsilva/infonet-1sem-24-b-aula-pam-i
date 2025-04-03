a025797bb88f9fd7480aa1e71a6677731d0c73c0
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2025-04-02 22:51:24 -0300

    exemplo 3

---
+++ src/exemplos/exemplo-03/index.js
@@ -0,0 +1,52 @@
+import { View, Text, Button, Alert } from 'react-native';
+
+import styles from './styles';
+
+function Exemplo03() {
+
+    function handleExibeBotao() {
+        Alert.alert('Alerta', 'Alguém clicou no botão!');
+    }
+
+    const handleExibeBotao2 = () => {
+        Alert.alert(
+            'Alerta',
+            'Alguém clicou no botão!',
+            [
+                {
+                    text: 'Sim',
+                    onPress: () => Alert.alert('Mensagem', 'Clicou em sim'),
+                    style: 'default',
+                },
+                {
+                    text: 'Não',
+                    onPress: () => Alert.alert('Clicou em não'),
+                    style: 'default',
+                },
+                {
+                    text: 'Cancelar',
+                    onPress: () => Alert.alert('Clicou em cancelar'),
+                    style: 'default',
+                },
+            ]
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Exemplo 3</Text>
+            <Text style={styles.texto}>Variáveis e State</Text>
+
+            <View style={styles.botaoAlert}>
+                <Button
+                    title='Não clique'
+                    color={'darkslategrey'}
+                    onPress={handleExibeBotao2}
+                />
+            </View>
+
+        </View>
+    );
+}
+
+export default Exemplo03;
